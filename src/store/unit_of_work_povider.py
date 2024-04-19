47f35b1c2afde0b2dc75bc2709863190614ce3ed
Oliver Shetler <oliver@olivershetler.com>
2024-04-19 13:10:33 -0700

    Added a type checking line to the UnitOfWorProvicer __call__ method. Added a string conversion to the demo jupyter notebook when making a uow from unit of work string.

--- src/store/unit_of_work_povider.py
+++ src/store/unit_of_work_povider.py
@@ -19,6 +19,8 @@ class UnitOfWorkProvider:
         self._memory_store = memory_store
 
     def __call__(self, project_name):
+        if not isinstance(project_name, str):
+            raise ValueError("project_name must be a string")
         model_dao = MongoDAO(client=self._mongo_client,
                             database_name=project_name,
                             collection_name='domain_models',
@@ -35,7 +37,7 @@ class UnitOfWorkProvider:
         in_memory_object_dao = InMemoryObjectDAO(memory_store=self._memory_store)
 
         domain_model_repo = DomainModelRepository(model_dao=model_dao)
-        
+
         data_repo = DataRepository(record_dao=record_dao,
                                 file_dao=file_system_dao,
                                 domain_repo=domain_model_repo)
